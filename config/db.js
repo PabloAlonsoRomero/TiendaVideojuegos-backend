30b19046fd86a6b5b8212a6b0236081a05c62dc2
Pablo Alonso <pabloealonsor@gmail.com>
2024-12-02 19:40:46 -0600

    fix: update database connection string to use local MongoDB instance

--- config/db.js
+++ config/db.js
@@ -7,7 +7,8 @@ const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_NAME = process.env.DB_NAME;
 const DB_CLUSTER = process.env.DB_CLUSTER;
-const conexion = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_CLUSTER}/${DB_NAME}?retryWrites=true&w=majority`;
+//const conexion = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_CLUSTER}/${DB_NAME}?retryWrites=true&w=majority`;
+const conexion = `mongodb://localhost:27017/tienda`
 
 const connectDB = async () => {
     try {
