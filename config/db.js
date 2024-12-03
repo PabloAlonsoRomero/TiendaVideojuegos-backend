6d5eaff2dce3538c19ce96696428a44a96aca340
Pablo Alonso <pabloealonsor@gmail.com>
2024-12-02 20:54:56 -0600

    fix: switch MongoDB connection string to use local container in db.js

--- config/db.js
+++ config/db.js
@@ -7,8 +7,8 @@ const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_NAME = process.env.DB_NAME;
 const DB_CLUSTER = process.env.DB_CLUSTER;
-const conexion = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_CLUSTER}/${DB_NAME}?retryWrites=true&w=majority`;
-//const conexion = `mongodb://mongo_container:27017/tienda`
+//const conexion = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_CLUSTER}/${DB_NAME}?retryWrites=true&w=majority`;
+const conexion = `mongodb://mongo_container:27017/tienda`
 
 const connectDB = async () => {
     try {
